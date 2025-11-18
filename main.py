8d53ba588270d86c7d849e1995108fe34c164c7c
obogeanu2712 <obogeanumateirazvan@gmail.com>
2025-11-18 17:07:58 +0200

    downloaded caltech256

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("sugi pula")
+from torchvision.datasets import Caltech256
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+
+train_dataset = Caltech256(
+    root = './',
+    transform = ToTensor(),
+    target_transform=ToTensor(),
+    download=True
+)
+
+test_dataset = Caltech256(
+    root='./',
+    transform=ToTensor(),
+    target_transform=ToTensor(),
+    download=True
+)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=64,
+    shuffle=True
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=64,
+    shuffle=False
+)
+
